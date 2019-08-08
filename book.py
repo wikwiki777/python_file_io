b5f806b9bbdb6d1d2ccaee6aca842c8cbf972045
wicked777 <wicky.akloe@gmail.com>
2019-08-08 19:58:59 +0200

    Added book.py program to count words in book.txt

---
+++ book.py
@@ -0,0 +1,25 @@
+# Import the regular expressions library
+import re
+# Import collections library to count words
+import collections
+
+"""
+ Here we'll read everything into a string called text use the read() method.
+Have it all in lowercase
+"""
+text = open("book.txt").read().lower()
+
+"""
+The pattern we're using is \w+
+The w denotes anything that's not a whitespace, and the plus denotes one or more. So, for every
+occurrence of one or more characters that are not whitespace, we view that as
+a word. We may get some false positives - it's not perfect but it works fine for
+"""
+words = re.findall("\w+", text)
+
+"""
+Then, the counter() method from collections counts how many
+occurrences there are and the most_common() method limits the results to ten
+words
+"""
+print(collections.Counter(words).most_common(10))
